b8e1a20b78bb7e275e931c0ada72c45a9b65af4b
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-08-18 01:38:49 +0530

    blocking and non-blocking execution

---
+++ node/6n.js
@@ -0,0 +1,13 @@
+// Synchronous or blocking-
+// -Line by line execution
+
+// Asynchronous or nonblocking
+// line by line execution not guranteed
+// -callbacks will fire
+
+
+const fs =require("fs");
+let text = fs.readFile("dele.txt", "utf-8", (error, data)=>{
+    console.log(error, data);
+});
+console.log("this is a message");
