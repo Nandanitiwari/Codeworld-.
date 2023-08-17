126672b689db8803bb3deecaca5126f0f8cd737e
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-08-18 01:20:40 +0530

    nodejs module

---
+++ node/5n.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let text = fs.readFileSync("Dele.txt", "utf-8");
+text = text.replace("browserr" , "rohan");
+console.log("the content of the file is ");
+console.log(text);
+
+console.log("Creating a new file");
+fs.writeFileSync("rohan.txt", text);
