1a37cb69398ac60b204cc4b47d8551df4b23fca8
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-08-18 01:11:02 +0530

    Nodejs module with example

--- node/4n.js
+++ node/4n.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
-const text = fs.readFileSync("dele.txt", "utf-8");
+const text = fs.readFileSync("Dele.txt", "utf-8");
 console.log("the content of the file is ");
 console.log(text);
