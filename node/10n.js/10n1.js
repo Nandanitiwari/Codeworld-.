5b5211f6fa602152ddac7d47772b6b90d12ac550
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-08-24 22:56:56 +0530

    node package manager

--- node/10n.js/10n1.js
+++ node/10n.js/10n1.js
@@ -1,6 +1,7 @@
 console.log("this is npm tutorial 69");
 console.log("nodemon is used to directly reflect changes in our browse when we save the changes in our code");
-// versioning of software
+
+// Versioning of software
 // "1.0.0"
 // major.minor.patch
 // extract old feature.new-function(new features without breaking old features).fix-bug
