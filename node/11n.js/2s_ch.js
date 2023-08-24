581d7e5bfe4be2ec1ffb5521bc95d8baa8499a06
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-08-25 00:24:50 +0530

    synchronous operations

--- node/11n.js/2s_ch.js
+++ node/11n.js/2s_ch.js
@@ -13,13 +13,20 @@ const fs = require('fs');
 // fs.mkdirSync("thapa");
 
 //creating new file in thapa folder
-// fs.writeFileSync("bio.txt", "creating new file in thapa folder ");
+// fs.writeFileSync("thapa/bio.txt", "creating new file in thapa folder ");
 
 //add more data into file without overriding the existing data 
-// fs.appendFileSync("bio.txt", "add more data into file without overriding the existing data ");
+// fs.appendFileSync("thapa/bio.txt", "add more data into file without overriding the existing data ");
 
 // 4. read the data without getting the buffer data at first 
-// const buf_data = fs.readFileSync("bio.txt")
+// const buf_data = fs.readFileSync("thapa/bio.txt")
 // org_data = buf_data.toString();
 // console.log(org_data);
+
+// 4. read the data without getting the buffer data at first 
+//  const data = fs.readFileSync("thapa/bio.txt", "utf-8")
+//  console.log(data);
+
+// 5. rename the file name to mybio.txt 
+fs.renameSync("thapa/bio.txt" , "thapa/mybio.txt");
 
