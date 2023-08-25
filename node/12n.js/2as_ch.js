4b61df01aaec0180f8904cc08650d100d2972062
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-08-25 11:34:03 +0530

    asynchronous operation challange

--- node/12n.js/2as_ch.js
+++ node/12n.js/2as_ch.js
@@ -8,17 +8,17 @@
 // 6. now delete both the file and folder 
 
 // 1. create a folder name thapa
-const { error } = require("console");
+// const { error } = require("console");
 const fs = require ("fs");
 // fs.mkdir("thapa", (err) => {
 //     console.log("folder created");
 // })
 
 // 2. create a file in it named read.txt add data into it
-// fs.writeFile("bio.txt", "creating file", (error) =>{
-//     console.log("data added");
-//     console.log(error);
-// })
+fs.writeFile("thapa/bio.txt", "creating file", (error) =>{
+    console.log("data added");
+    console.log(error);
+})
 
 // 3. add more data into file without overriding the existing data 
 // fs.appendFile("bio.txt", "  |  appending data", (error) => {
@@ -38,6 +38,6 @@ const fs = require ("fs");
 // })
 
 // 6. now delete both the file and folder 
-fs.rmdir("thapa", (error,data) => {
-    console.log("folder/directory  deleted");
-})
+// fs.rmdir("thapa", (error,data) => {
+//     console.log("folder/directory  deleted");
+// })
