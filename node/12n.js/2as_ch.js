5c6113bdb8b3c010b0c85398731a496690931c06
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-08-25 11:25:13 +0530

    asynchronous operation challange

---
+++ node/12n.js/2as_ch.js
@@ -0,0 +1,32 @@
+// CHALLANGE
+
+// 1. create a folder name thapa
+// 2. create a file in it named bio.txt add data into it
+// 3. add more data into file without overriding the existing data 
+// 4. read the data without getting the buffer data at first 
+// 5. rename the file name to mybio.txt 
+// 6. now delete both the file and folder 
+
+// 1. create a folder name thapa
+const fs = require ("fs");
+// fs.mkdir("thapa", (err) => {
+//     console.log("folder created");
+// })
+
+// 2. create a file in it named read.txt add data into it
+// fs.writeFile("bio.txt", "creating file", (error) =>{
+//     console.log("data added");
+//     console.log(error);
+// })
+
+// 3. add more data into file without overriding the existing data 
+// fs.appendFile("bio.txt", "  |  appending data", (error) => {
+//     console.log("data appended");
+//     console.log(error);
+// })
+
+// 4. read the data without getting the buffer data at first 
+fs.readFile("bio.txt", "utf-8", (error ,data) =>{
+    console.log(data);
+    console.log(error);
+})
