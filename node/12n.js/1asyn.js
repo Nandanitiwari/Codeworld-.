29a5654ba15f0e1945ceef34a87d12fbad2fd2e0
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-08-25 11:11:27 +0530

    asynchronous operation

--- node/12n.js/1asyn.js
+++ node/12n.js/1asyn.js
@@ -1,9 +1,4 @@
-// 1. create a folder name thapa
-// 2. create a file in it named read.txt add data into it
-// 3. add more data into file without overriding the existing data 
-// 4. read the data without getting the buffer data at first 
-// 5. rename the file name to mybio.txt 
-// 6. now delete both the file and folder 
+//asynchronous 
 
 const { error } = require("console");
 const fs = require("fs");
@@ -22,7 +17,7 @@ const fs = require("fs");
 
 // });
 
-// 4. read the data without getting the buffer data at first 
+// 4. read the data 
 fs.readFile("read.txt", "utf-8", (error ,data) => {
     console.log(data);
 })
