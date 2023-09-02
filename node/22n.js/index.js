a44eb199c00809135302dbd984a81ff92cd4c3ae
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-09-02 11:49:02 +0530

    API

---
+++ node/22n.js/index.js
@@ -0,0 +1,3 @@
+//API is the acronym for Application Programming Interface, which is a software intermediary that allow two applications to talk to each other, Each time you use an app like Facebook, send an instant message, or check the weather on your phone, you're using an API
+
+//API is a serrvice which allow us to request data.
