2788c57da92c45c35e648ea8ab6880457a58ce50
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2023-09-02 11:30:09 +0530

    creating our own module

---
+++ node/15n.js/index.js
@@ -0,0 +1,8 @@
+const {add, sub, mul, dev, name}  = require("./oper");
+// const name = require("./oper");
+
+console.log(add(5, 5));
+console.log(sub(10, 5));
+console.log(mul(10, 5));
+console.log(dev(10, 5));
+console.log(name);
---
+++ node/15n.js/oper.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+    return a + b;
+};
+const sub = (a, b) => {
+    return a - b;
+};
+const mul = (a, b) => {
+    return a * b;
+};
+const dev = (a, b) => {
+    return a / b;
+};
+const name = "nannu";
+// module.exports.add = add;
+// module.exports.subb = sub;
+// module.exports.mul = mul;
+// module.exports.dev = dev;
+ module.exports = {add, sub, mul, dev, name};
+// module.exports = name;
